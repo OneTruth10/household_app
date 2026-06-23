e6fac726150155470f7b4f9993c3afb51ab80ea2
Cactus <firespiritkn10@gmail.com>
2026-06-23 19:30:07 +0100

    Fix seeds to avoid foreign key violation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
-# 既存のデータを一度削除して綺麗にする（重複登録を防ぐため）
-Currency.destroy_all
-
-# 通貨のマスターデータを作成
-Currency.create!(code: 'JPY', symbol: '¥')
-Currency.create!(code: 'USD', symbol: '$')
-Currency.create!(code: 'EUR', symbol: '€')
-Currency.create!(code: 'GBP', symbol: '£')
-
-puts "通貨データの作成が完了しました！ (JPY, USD, EUR, GBP)"
-
+# db/seeds.rb の中身
+Currency.find_or_create_by!(code: 'JPY') { |c| c.symbol = '¥' }
+Currency.find_or_create_by!(code: 'USD') { |c| c.symbol = '$' }
+Currency.find_or_create_by!(code: 'EUR') { |c| c.symbol = '€' }
+Currency.find_or_create_by!(code: 'GBP') { |c| c.symbol = '£' }
